ae666722e65b325c539e339c728ec3c4adc14a39
Danis Music <danism@student.chalmers.se>
2025-11-23 15:34:28 +0100

    feat: add log creation (#1)

--- src/speech-recognition-service/speech-recognition.py
+++ src/speech-recognition-service/speech-recognition.py
@@ -1,42 +1,48 @@
-from faster_whisper import WhisperModel
-import sounddevice as sd 
-import soundfile as sf 
-import numpy as np
-import time 
+import time
+import whisper as ws 
 
-SAMPLERATE = 16000
-CHUNK = 16000
-CHUNK_PROCESSING_LEN = 5
-CHUNK_OVERLAP_TIME = 1
-CHANNELS = 1
-FILENAME = "output.wav"
+MIN_PAUSE = 0.2
+model = ws.load_model("base")
 
-CHUNK_SAMPLES = int(CHUNK_PROCESSING_LEN * SAMPLERATE)
-OVERLAP_SAMPLES = int(CHUNK_OVERLAP_TIME * SAMPLERATE)
+start = time.time()
+result = model.transcribe("jacksepticeyeTest.wav", word_timestamps=True)
 
-model = WhisperModel("base", compute_type="int8")
+prev_end = None
+sentence_buffer = []
+sentence_start = None
+with open("log.txt", "x"):
+    pass
+log_file = open("log.txt", "a")
 
-buffer = np.zeros(0, dtype=np.float32)
+for segment in result["segments"]:
+    for word in segment["words"]:
+        word_start = word["start"]
+        word_end = word["end"]
+        text = word["word"].strip()
 
-def process_chunk(audio_chunk):
-    if len(audio_chunk) == 0:
-        return
-    segments, info = model.transcribe(audio_chunk, beam_size=5)
-    for segment in segments:
-        print(f"[{segment.start:.2f}s -> {segment.end:.2f}s] {segment.text}", flush=True)
+        if sentence_start is None:
+            sentence_start = word_start
 
-  
-def callback(indata, frames, time_info, status):
-    global buffer
-    audio_data = indata[:, 0].astype(np.float32).flatten()
-    buffer = np.concatenate([buffer, audio_data])
+        if prev_end is not None and (word_start - prev_end) > MIN_PAUSE:
+            sentence_end = prev_end
+            
+            dialouge_text = f"[{sentence_start:.2f}] --> [{sentence_end:.2f}]: {' '.join(sentence_buffer)}\n"
+            silence_text = f"[{prev_end:.2f}] --> [{word_start:.2f}]: . . .\n"
+            
+            log_file.write(dialouge_text)
+            log_file.write(silence_text)
+            
+            sentence_buffer = []
+            sentence_start = word_start
+        
+        sentence_buffer.append(text)
+        prev_end = word_end
+if sentence_buffer: 
+    sentence_end = prev_end
+    last_sentence = f"[{sentence_start:.2f}] --> [{sentence_end:.2f}]: {' '.join(sentence_buffer)}\n"
+    log_file.write(last_sentence)
 
-    while len(buffer) >= CHUNK_SAMPLES:
-        chunk_to_process = buffer[:CHUNK_SAMPLES]
-        process_chunk(chunk_to_process)
-        buffer = buffer[CHUNK_SAMPLES - OVERLAP_SAMPLES:]
+log_file.close()
 
-
-with sd.InputStream(channels=1, samplerate=SAMPLERATE, blocksize=CHUNK, callback=callback):
-    while True:
-        sd.sleep(1000)
+print(open("log.txt", "r").read())
+print(f"Time elapsed: {time.time() - start} seconds")
