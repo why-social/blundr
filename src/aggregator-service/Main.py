154a8fb314f73f129a362308fb918a60673149c4
Danis Music <danism@student.chalmers.se>
2025-12-05 13:57:02 +0100

    feat: allow for face emotion and voice emotion content to be sent in different formats (#8)

--- src/aggregator-service/Main.py
+++ src/aggregator-service/Main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Form, UploadFile, File
 import json
 from aggregator import aggregate_files
 app = FastAPI()
@@ -8,20 +8,33 @@ session_id_tracker = {}
 
 @app.post("/aggregator/")
 async def get_files(
-    session_id: str = Body(...),
-    uuid: str = Body(...),
-    content: str = Body(...)
+    session_id: str = Form(...),
+    uuid: str = Form(...),
+
+    fe_text: str | None = Form(None),
+    ve_text: UploadFile | None = File(None)
     ):
 
-    parsed_info = json.loads(content)
-
+   
     if session_id not in session_id_tracker:
         session_id_tracker[session_id] = {}
     if uuid not in session_id_tracker[session_id]:
         session_id_tracker[session_id][uuid] = {}
-    
-    for key, value in parsed_info.items():
-        session_id_tracker[session_id][uuid][key] = value
+
+    if fe_text:
+        try:
+            parsed_info = json.loads(fe_text)
+        except json.JSONDecodeError:
+            return {"status": 404, "message": "Invalid JSON"}
+
+        session_id_tracker[session_id][uuid]["face"] = parsed_info
+    elif ve_text:
+        raw_file = await ve_text.read()
+        content = raw.decode("utf-8")
+
+        session_id_tracker[session_id][uuid]["voice"] = content
+    else:
+        return {"status": 404, "message": "No content provided"}
 
     session_users = session_id_tracker[session_id]
 
