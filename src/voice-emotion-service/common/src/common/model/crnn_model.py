b527682ee6f0d2a55c0e8578ceb7e2780cd01008
Maxine Orlen <orlovs@student.chalmers.se>
2025-12-08 20:05:55 +0100

    refactor: prepare for alternative model (#6)

--- src/voice-emotion-service/common/src/common/model/crnn_model.py
+++ src/voice-emotion-service/common/src/common/model/crnn_model.py
@@ -1,7 +1,9 @@
+from common.config.dataset_config import DatasetConfig
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from common.config.model_config import ModelConfig
+from torch.utils.data import DataLoader
 
 
 class CRNNModel(nn.Module):
@@ -85,3 +87,85 @@ class CRNNModel(nn.Module):
         context = torch.sum(attn_weights * lstm_out, dim=1)
 
         return self.classifier(context)
+
+
+def train_crnn(model: CRNNModel, model_config: ModelConfig, data_config: DatasetConfig, train_loader: DataLoader, val_loader: DataLoader):
+    train_labels = [s.emotion for s in train_loader.dataset.samples]
+    class_counts = [train_labels.count(k) for k in data_config.label_map.keys()]
+    total_samples = sum(class_counts)
+    weights = [
+        total_samples / (len(class_counts) * c) if c > 0 else 0 for c in class_counts
+    ]
+
+    class_weights = torch.FloatTensor(weights).to(model_config.device)
+    print(f"Class Weights: {weights}")
+    criterion = nn.CrossEntropyLoss(weight=class_weights)
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=model_config.learning_rate,
+        weight_decay=model_config.weight_decay,
+    )
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode="max", factor=0.5, patience=3
+    )
+
+    best_val_acc = 0.0
+
+    for epoch in range(model_config.epochs):
+        model.train()
+        running_loss = 0.0
+        correct = 0
+        total = 0
+
+        print(f"\nEpoch {epoch + 1}/{model_config.epochs}")
+
+        for inputs, labels in train_loader:
+            inputs, labels = (
+                inputs.to(model_config.device),
+                labels.to(model_config.device),
+            )
+
+            optimizer.zero_grad()
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+        epoch_acc = 100 * correct / total
+        print(
+            f"Train Loss: {running_loss / len(train_loader):.4f} | Acc: {epoch_acc:.2f}%"
+        )
+
+        # VALIDATION
+        model.eval()
+        val_correct = 0
+        val_total = 0
+        with torch.no_grad():
+            for inputs, labels in val_loader:
+                inputs, labels = (
+                    inputs.to(model_config.device),
+                    labels.to(model_config.device),
+                )
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                val_total += labels.size(0)
+                val_correct += (predicted == labels).sum().item()
+
+        val_acc = 100 * val_correct / val_total
+        print(f"Validation Acc: {val_acc:.2f}%")
+
+        scheduler.step(val_acc)
+
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            print("New Best.")
+            if val_acc >= model_config.save_thresh:
+                torch.save(model.state_dict(), model_config.out_path)
+                print("Saved.")
--- src/voice-emotion-service/train/main.py
+++ src/voice-emotion-service/train/main.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from common.config.dataset_config import AugmentsConfig, DatasetConfig
 from common.config.model_config import ModelConfig
-from common.model.crnn_model import CRNNModel
+from common.model.crnn_model import CRNNModel, train_crnn
 from dataset.augment import augment_dataset
 from dataset.dataloaders import create_dataloaders
 from dataset.download import download_datasets
@@ -26,88 +26,12 @@ if __name__ == "__main__":
         dataset, MODEL_CONFIG, AUGMENTS_CONFIG
     )
 
-    train_labels = [s.emotion for s in train_loader.dataset.samples]
-    class_counts = [train_labels.count(k) for k in DATASET_CONFIG.label_map.keys()]
-    total_samples = sum(class_counts)
-    weights = [
-        total_samples / (len(class_counts) * c) if c > 0 else 0 for c in class_counts
-    ]
-    class_weights = torch.FloatTensor(weights).to(MODEL_CONFIG.device)
-    print(f"Class Weights: {weights}")
 
     model = CRNNModel(
         MODEL_CONFIG, DATASET_CONFIG.n_mels, DATASET_CONFIG.n_classes
     ).to(MODEL_CONFIG.device)
-    criterion = nn.CrossEntropyLoss(weight=class_weights)
-    optimizer = torch.optim.Adam(
-        model.parameters(),
-        lr=MODEL_CONFIG.learning_rate,
-        weight_decay=MODEL_CONFIG.weight_decay,
-    )
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, mode="max", factor=0.5, patience=3
-    )
 
-    best_val_acc = 0.0
-
-    for epoch in range(MODEL_CONFIG.epochs):
-        model.train()
-        running_loss = 0.0
-        correct = 0
-        total = 0
-
-        print(f"\nEpoch {epoch + 1}/{MODEL_CONFIG.epochs}")
-
-        for inputs, labels in train_loader:
-            inputs, labels = (
-                inputs.to(MODEL_CONFIG.device),
-                labels.to(MODEL_CONFIG.device),
-            )
-
-            optimizer.zero_grad()
-
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.item()
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-        epoch_acc = 100 * correct / total
-        print(
-            f"Train Loss: {running_loss / len(train_loader):.4f} | Acc: {epoch_acc:.2f}%"
-        )
-
-        # VALIDATION
-        model.eval()
-        val_correct = 0
-        val_total = 0
-        with torch.no_grad():
-            for inputs, labels in val_loader:
-                inputs, labels = (
-                    inputs.to(MODEL_CONFIG.device),
-                    labels.to(MODEL_CONFIG.device),
-                )
-                outputs = model(inputs)
-                _, predicted = torch.max(outputs.data, 1)
-                val_total += labels.size(0)
-                val_correct += (predicted == labels).sum().item()
-
-        val_acc = 100 * val_correct / val_total
-        print(f"Validation Acc: {val_acc:.2f}%")
-
-        scheduler.step(val_acc)
-
-        if val_acc > best_val_acc:
-            best_val_acc = val_acc
-            print("New Best.")
-            if val_acc >= MODEL_CONFIG.save_thresh:
-                torch.save(model.state_dict(), MODEL_CONFIG.out_path)
-                print("Saved.")
+    train_crnn(model, MODEL_CONFIG, DATASET_CONFIG, train_loader, val_loader)
 
     model.load_state_dict(torch.load(MODEL_CONFIG.out_path))
     model.eval()
