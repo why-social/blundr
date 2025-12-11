3880afdf4008ef2614ccf2faf64bc83336254f10
Razvan Albu <razvana@student.chalmers.se>
2025-12-11 13:32:39 +0100

    feat: prepare analyze service request flow (#20)

---
+++ src/frontend/app/api/proxy.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export type Context = { params: Promise<{ path?: string[] }> };
+interface NodeFetchInit extends RequestInit {
+  duplex?: "half";
+}
+
+export async function proxy(
+  location: string,
+  req: NextRequest,
+  context: Context,
+) {
+  const { path } = await context.params;
+  const backendUrl = `${location}/${path?.join("/") ?? ""}`;
+
+  // node 18+ requires a duples attribute to be set
+  const init: NodeFetchInit = {
+    method: req.method,
+    headers: req.headers,
+  };
+
+  if (!["GET", "HEAD"].includes(req.method)) {
+    init.body = req.body;
+    init.duplex = "half";
+  }
+
+  const backendRes = await fetch(backendUrl, init);
+
+  const data = await backendRes.arrayBuffer();
+  const res = new NextResponse(Buffer.from(data), {
+    status: backendRes.status,
+  });
+
+  backendRes.headers.forEach((value, key) => {
+    res.headers.set(key, value);
+  });
+
+  return res;
+}
