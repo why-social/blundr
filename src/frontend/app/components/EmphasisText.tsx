f7b8d419f293881a0cd12e6a3afda06c07ae921c
Razvan Albu <razvana@student.chalmers.se>
2025-12-12 12:06:21 +0100

    feat: made it pretty :D (#20)

---
+++ src/frontend/app/components/EmphasisText.tsx
@@ -0,0 +1,42 @@
+import { twMerge } from "tailwind-merge";
+
+export const EmphasisText = ({
+  text,
+  emphasis,
+  className = "",
+}: {
+  text: string;
+  emphasis?: "strong" | "weak";
+  className?: string;
+}) => {
+  return (
+    <div className={`relative inline-block ${className}`}>
+      <h1 className="invisible">{text}</h1>
+
+      {emphasis === "strong" && (
+        <>
+          <h1 className="absolute -top-3 -left-3 text-red-600 select-none">
+            {text}
+          </h1>
+
+          <h1 className="absolute -top-2 -left-2 text-red-400 select-none">
+            {text}
+          </h1>
+        </>
+      )}
+
+      <h1
+        className={twMerge(
+          "absolute select-none",
+          emphasis === "strong"
+            ? "-top-1 -left-1 text-pink-300"
+            : "-top-0.5 -left-0.5 text-red-600",
+        )}
+      >
+        {text}
+      </h1>
+
+      <h1 className="text-white-500 absolute top-0">{text}</h1>
+    </div>
+  );
+};
