b91f6acd157f4b8fb518c40911214363deaa436a
Razvan Albu <razvana@student.chalmers.se>
2025-12-13 22:18:09 +0100

    fix: improve stability of video ffmpeg encoding (#20)

--- src/chatroom/src/middleware/errorHandler.ts
+++ src/chatroom/src/middleware/errorHandler.ts
@@ -1,4 +1,5 @@
 import { type Request, type Response, type NextFunction } from "express";
+import { logError } from "../utils/logs.js";
 
 export interface AppError extends Error {
   status?: number;
@@ -10,7 +11,7 @@ export default (
   res: Response,
   _next: NextFunction
 ) => {
-  console.error(err);
+  logError(err);
 
   res.status(err.status || 500).json({
     message: err.message || "Internal Server Error",
