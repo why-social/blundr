961a18feaa759b0467cd655204f26366fe975cd7
Maxine Orlen <orlovs@student.chalmers.se>
2025-12-14 12:30:51 +0100

    fix: build script (#28)

--- k8s/build-push.sh
+++ k8s/build-push.sh
@@ -44,10 +44,8 @@ docker buildx build \
   --platform linux/amd64 \
   -t ${KUBE_REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${SERVICE_NAME}:v1 \
   -t ${KUBE_REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${SERVICE_NAME}:latest \
-  "$BUILD_CONTEXT"
+  --push "$BUILD_CONTEXT"
 
 # Push Docker image
-docker push ${KUBE_REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${SERVICE_NAME}:v1
-docker push ${KUBE_REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${SERVICE_NAME}:latest
 
 echo "Docker images for '$SERVICE_NAME' built and pushed successfully."
