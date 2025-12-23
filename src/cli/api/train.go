82dd73becda103847cdbfd3ddd20bdd14963fc7a
Razvan Albu <razvana@student.chalmers.se>
2025-12-23 14:29:03 +0200

    feat: implement model training client request (#33)

--- src/cli/api/train.go
+++ src/cli/api/train.go
@@ -1,6 +1,34 @@
 package api
 
+import (
+	"blundr-cli/utils"
+	"fmt"
+)
+
 func TrainModel(client *Client) error {
-	// TODO
+	response, err := utils.RunWithLoadingAnimation("Submitting training job",
+		func() (*StartTrainingResponse, error) {
+			return client.StartTraining()
+		})
+
+	if err != nil {
+		return fmt.Errorf("API request failed: %w", err)
+	}
+
+	fmt.Println("\n--- Training Job Submitted ---")
+	fmt.Printf("Model Version:       %s\n", response.ModelVersion)
+	fmt.Printf("Training Set Size:   %d samples\n", response.TrainingSetSize)
+
+	if response.JobStatus != nil {
+		fmt.Printf("Succeeded Pods:      %d\n", response.JobStatus.Succeeded)
+		fmt.Printf("Failed Pods:         %d\n", response.JobStatus.Failed)
+		fmt.Printf("Active Pods:         %d\n", response.JobStatus.Active)
+	}
+
+	fmt.Println("Job metadata:")
+	fmt.Printf("  Name:        %s\n", response.Metadata.Name)
+	fmt.Printf("  Namespace:   %s\n", response.Metadata.Namespace)
+	fmt.Printf("  CreationTime:%s\n", response.Metadata.CreationTimestamp.String())
+
 	return nil
 }
